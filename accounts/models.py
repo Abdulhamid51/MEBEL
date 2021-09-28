49db5cb7087c0f3ed7e023a3294ca3c1c121f93c
Abdulhamid <73017095+Abdulhamid51@users.noreply.github.com>
2021-09-28 17:24:26 +0300

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,13 @@
 from django.db import models
-from django.auth
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Saved(models.Model):
+    product = models.ManyToManyField(Product, related_name='saved')
+
 class UserProfile(models.Model):
-    user = 
+    user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE)
+    saved = models.OneToOneField(Saved, related_name='saver', on_delete=models.CASCADE)
+
+class 
