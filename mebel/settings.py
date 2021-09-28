3c1f2051f91b5b0a3e642234c536760790b59788
Mr-Muhammadjon <76444665+Mr-Muhammadjon@users.noreply.github.com>
2021-09-28 19:26:52 +0500

    tempaltes connect

--- mebel/settings.py
+++ mebel/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
