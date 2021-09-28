8d50182ceab045c6838be7676061af6b72ca0171
Abdulhamid <73017095+Abdulhamid51@users.noreply.github.com>
2021-09-28 22:31:34 +0300

    ozgina xato bolib qopti

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField("nomi", max_length=150)
-    slug = models.models.SlugField("slug")
+    slug = models.SlugField("slug")
 
     def __str__(self):
         return self.title
